3e8b71ed1d82769c067d35443d2070c5f0a5d749
koosco <rnxogud123@naver.com>
2024-12-02 01:28:56 +0900

    initial commit

---
+++ src/main/java/bigtech/fbai/FbaiApplication.java
@@ -0,0 +1,13 @@
+package bigtech.fbai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbaiApplication.class, args);
+    }
+
+}

3f1f55136bd3add08cadacec589b0a6e08edb45b
koosco <rnxogud123@naver.com>
2024-12-09 04:17:38 +0900

    fix: 사용자 image url 길이 수정
    image url이 길어서 길이 초과가 되던 에러를 수정했습니다.

--- src/main/java/bigtech/fbai/member/dao/entity/Member.java
+++ src/main/java/bigtech/fbai/member/dao/entity/Member.java
@@ -2,6 +2,7 @@ package bigtech.fbai.member.dao.entity;
 
 import bigtech.fbai.common.exception.CommonException;
 import bigtech.fbai.common.exception.ErrorCode;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +27,7 @@ public class Member {
 
     private String introduce;
 
+    @Column(length = 500)
     private String imageUrl;
 
     public void update(String nickname, String imageUrl, String introduce) {
